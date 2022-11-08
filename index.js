62c80f3fd5d40e0f88ce29d2b7aef09eafd7a0bc
sagar-kpr <102784500+sagar-kpr@users.noreply.github.com>
2022-11-08 12:05:01 +0530

    added sass-middleware

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const port = 5000;
 const db = require('./config/mongoose');
-
 const app = express();
-
-
-
+const parser = require('body-parser');
+const sassMiddlware = require('node-sass-middleware');
 const expressLayout = require('express-ejs-layouts');
+const path = require('path');
 
 
 
+app.use(sassMiddlware({
+    src: path.join(__dirname, './assets', 'scss'),
+    dest :path.join(__dirname, './assets', 'css'),
+    outputStyle: 'expanded',
+    prefix: '/css'
+}));
+
+app.use(parser.urlencoded({extended: false}));
 app.use(express.static('./assets'));
-
 app.use(expressLayout);
 app.set('view engine', 'ejs');
 app.set('views', './view');
