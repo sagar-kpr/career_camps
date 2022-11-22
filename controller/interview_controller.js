9c8e05a9607e362d80118bff8727672d7602628f
sagar-kpr <102784500+sagar-kpr@users.noreply.github.com>
2022-11-22 16:57:15 +0530

    fetch jobs

--- controller/interview_controller.js
+++ controller/interview_controller.js
@@ -1,9 +1,17 @@
-const User = require('../models/schema');
-const StudentBasic = require('../models/student_basic_schema');
 
+const { $where } = require('../models/student_basic_schema');
+const StudentBasic = require('../models/student_basic_schema');
 module.exports.inteview = async function(req,res){
     let studentId = await StudentBasic.findById(req.params.id);
-    console.log('ttt', studentId)
+   // console.log('ttt', studentId)
+
+    /*await fetch('https://remotive.io/api/remote-jobs?category=software-dev')
+    .then( (res) => res.json())
+    .then( (json) => {
+        console.log('eweqewqeqweq',json)
+    } )*/
+
+   
     return res.render('interview',{
         profile: studentId
 
