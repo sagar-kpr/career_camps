41a6c97a125395fae6bee1bd6f728e09c877dfba
sagar-kpr <sagarkpr6@gmail.com>
2022-11-22 19:48:53 +0530

    some

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,37 +1,43 @@
-$(function(){
-  console.log('hi',$(' .progress-pie-chart1') )
-  var $ppc1 = $(' .progress-pie-chart1')
-  var $ppc2 = $(' .progress-pie-chart2')
-  var $ppc3 = $(' .progress-pie-chart3')
 
-  percent1 = parseInt($ppc1.data('percent'))
+
+function pie(e){
+  console.log('0000',e)
+  var ppc1 = document.getElementById('progress-pie-chart1');
+  var ppc2 = document.getElementById('progress-pie-chart2');
+  var ppc3 = document.getElementById('progress-pie-chart3');
+
+  console.log(ppc1)
+  percent1 = parseInt(ppc1.getAttribute('data-percent'))
+  console.log('1212',percent1)
   deg1 = 360*percent1/100;
  
   if (percent1 > 50) {
-    $ppc1.addClass('gt-50');
-  }else{
+    ppc1.classList.add('gt-50');
+  }/*else{
     $ppc1.addClass('gt-49');
-  }
+  }*/
 
-  percent2 = parseInt($ppc2.data('percent'))
+  percent2 = parseInt(ppc2.getAttribute('data-percent'))
+  console.log(percent2)
   deg2 = 360*percent2/100;
   
 
   if (percent2 > 50) {
-    $ppc2.addClass('gt-50');
-  }else{
+    ppc2.classList.add('gt-50');
+  }/*else{
     $ppc2.addClass('gt-49');
-  }
+  }*/
 
 
-  percent3 = parseInt($ppc3.data('percent'))
+  percent3 = parseInt(ppc3.getAttribute('data-percent'))
+  console.log(percent3)
   deg3 = 360*percent3/100;
 
   if (percent3 > 50) {
-    $ppc3.addClass('gt-50');
-  }else{
+    ppc3.classList.add('gt-50');
+  }/*else{
     $ppc3.addClass('gt-49');
-  }
+  }*/
   
 
   $('.ppc-progress-fill1').css('transform','rotate('+ deg1 +'deg)');
@@ -42,6 +48,6 @@ $(function(){
  // $('.ppc-percents2 span').html(percent2+'%');
  // $('.ppc-percents3 span').html(percent3+'%');
   
-});
+};
 
 
