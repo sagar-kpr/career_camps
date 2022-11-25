076bf006300567a290dc747466b567535e870706
sagar-kpr <102784500+sagar-kpr@users.noreply.github.com>
2022-11-25 18:25:27 +0530

    created csv file

--- config/mongoose.js
+++ config/mongoose.js
@@ -10,3 +10,38 @@ db.once('open', function(){
     console.log('student db connected to database');
 })
 
+const mongodb = require("mongodb").MongoClient;
+
+let url = "mongodb://localhost/studentDB";
+const fastcsv = require('fast-csv')
+const fs = require('fs');
+const ws = fs.createWriteStream('student.csv');
+
+mongodb.connect(
+  url,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err, client) => {
+    if (err) throw err;
+
+    client
+      .db('studentDB')
+      .collection("studentbasics")
+      .find({})
+      .toArray((err, data) => {
+        if (err) throw err;
+
+        console.log('eww',data);
+        
+        // TODO: write data to CSV file
+        fastcsv
+          .write(data, { headers: true })
+          .on("finish", function() {
+            console.log("Write to student.csv successfully!");
+          })
+          .pipe(ws);
+
+        client.close();
+      });
+  }
+);
+
